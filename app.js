070e5809d208a8d6fa6719aa221c75b606884421
Venix <30481900+venix12@users.noreply.github.com>
2020-06-16 01:44:11 +0200

    remove a buch of unnecessary cookie things, don't use mongo

--- app.js
+++ app.js
@@ -1,27 +1,14 @@
 const config = require('./config.json');
-const cookieParser = require('cookie-parser');
 const express = require('express');
 const index = require('./index');
-const mongoose = require('mongoose');
 const session = require('express-session');
 
-const MongoStore = require('connect-mongo')(session);
-
 const app = express();
-
-mongoose.connect(config.db_string);
-
-mongoose.Promise = global.Promise;
-
-const db = mongoose.connection;
-
-app.use(cookieParser());
 
 app.use(session({
     secret: config.session_secret,
     resave: false,
     saveUninitialized: true,
-    store: new MongoStore({ mongooseConnection: db }),
 }));
 
 app.use('/', index);
