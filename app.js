3f3242b6c4963b2173fa427489227f01da40c4f6
Snowleopard-osu <49334335+Snowleopard-osu@users.noreply.github.com>
2020-06-15 19:47:25 -0500

    Enable SSL in victoria-auth

--- app.js
+++ app.js
@@ -2,6 +2,13 @@ const config = require('./config.json');
 const express = require('express');
 const index = require('./index');
 const session = require('express-session');
+const https = require('https');
+const fs = require('fs');
+
+var options = {
+  key: fs.readFileSync('/etc/letsencrypt/live/aiess.tk/privkey.pem'),
+  cert: fs.readFileSync('/etc/letsencrypt/live/aiess.tk/cert.pem')
+};
 
 const app = express();
 
@@ -14,3 +21,5 @@ app.use(session({
 app.use('/', index);
 
 app.listen(8080);
+
+https.createServer(options, app).listen(8443);
